3195275a386811a77eeb7378b7280512194101ff
joao carlos de oliveira dos santos <cj956254@gmail.com>
2023-10-06 22:09:09 -0300

    Mensagem do commit

--- app.js
+++ app.js
@@ -3,14 +3,42 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
+// Middleware para processar dados do formulário
 app.use(bodyParser.urlencoded({ extended: false }));
+
+// Servir arquivos estáticos (por exemplo, seu arquivo HTML)
 app.use(express.static('public'));
 
+// Lista para armazenar as confirmações
+const confirmacoes = [];
+
+// Rota para processar solicitações POST do formulário
 app.post('/confirm', (req, res) => {
     const name = req.body.name;
+    const novaConfirmacao = {
+        nome: name,
+        data: new Date().toLocaleString(),
+    };
+    confirmacoes.push(novaConfirmacao);
     res.send(`Olá, ${name}! Sua presença foi confirmada.`);
 });
 
+// Rota SSE para enviar eventos de confirmação ao cliente
+app.get('/sse', (req, res) => {
+    res.setHeader('Content-Type', 'text/event-stream');
+    res.setHeader('Cache-Control', 'no-cache');
+
+    const sendSSE = (data) => {
+        res.write(`data: ${JSON.stringify(data)}\n\n`);
+    };
+
+    // Enviar todas as confirmações armazenadas na lista ao cliente
+    confirmacoes.forEach((confirmacao) => {
+        sendSSE({ confirmation: confirmacao });
+    });
+});
+
+// Inicie o servidor
 app.listen(port, () => {
     console.log(`Servidor rodando em http://localhost:${port}`);
 });
--- server.js
+++ server.js
@@ -1,18 +1,42 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs'); // Módulo para manipular arquivos
 const app = express();
-const port = 3000; // Escolha a porta que deseja usar
+const port = 3000;
 
-// Middleware para processar dados do formulário
 app.use(bodyParser.urlencoded({ extended: false }));
-
-// Servir arquivos estáticos (por exemplo, seu arquivo HTML)
 app.use(express.static('public'));
 
 // Rota para processar solicitações POST do formulário
 app.post('/confirm', (req, res) => {
     const name = req.body.name;
+    const email = req.body.email; // Modifiquei para 'email' aqui
+
+    // Crie um objeto de confirmação com nome e email
+    const novaConfirmacao = {
+        nome: name,
+        email: email, // Modifiquei para 'email' aqui
+        data: new Date().toLocaleString(),
+    };
+
+    // Lê as confirmações existentes no arquivo JSON
+    let confirmacoes = [];
+    try {
+        const data = fs.readFileSync('confirmations.json', 'utf8');
+        confirmacoes = JSON.parse(data);
+    } catch (err) {
+        console.error('Erro ao ler o arquivo JSON:', err);
+    }
+
+    // Adiciona a nova confirmação à lista
+    confirmacoes.push(novaConfirmacao);
+
+    // Escreve as confirmações atualizadas no arquivo JSON
+    fs.writeFileSync('confirmations.json', JSON.stringify(confirmacoes, null, 2));
+
     res.send(`Olá, ${name}! Sua presença foi confirmada.`);
+
+    console.log(`${name} (${email}) confirmou a presença.`); // Modifiquei para 'email' aqui
 });
 
 // Inicie o servidor
